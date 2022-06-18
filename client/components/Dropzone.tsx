d1f071b222df3c08f00b02ffdf6cab4dad593bd3
Liedel <liedel75@gmail.com>
2022-06-18 12:34:06 +0200

    fix syntax condition css

--- client/components/Dropzone.tsx
+++ client/components/Dropzone.tsx
@@ -6,13 +6,33 @@ const Dropzone = () => {
         console.log(acceptedFiles);
     }, []);
 
-    const { getRootProps, getInputProps } = useDropzone({onDrop});
+    const { getRootProps, getInputProps, isDragAccept, isDragReject } = useDropzone({
+        onDrop,
+        multiple: false,
+        accept: {
+            'image/*': [".jpeg", ".png"],
+            'audio/*': [".mpeg", ".mp3"]
+        }
+    });
 
     return (
-        <div>
-            <div {...getRootProps()} className="w-full h-80">
+        <div className="p-4 w-full">
+            <div {...getRootProps()} className="w-full rounded-md cursor-pointer h-80 focus:outline-none">
                 <input {...getInputProps()} />
-                <p>Drag & Drop Files Here</p>
+                <div className={`flex flex-col items-center justify-center h-full space-y-3 border-2 border-dashed border-yellow-light rounded-xl
+                ${isDragReject === true ? 'bg-red-500' : ''}` + `${isDragAccept === true ? 'bg-green-500' : ''}`}
+                    >
+                    <img src="/images/folder.png" alt="folder" className="w-16 h-16"/>
+                    {isDragReject ? (
+                        <p className="text-red-500">Sorry, only supports images and mp3</p>
+                    ) : (
+                        <div>
+                            <p>Drag & Drop Files Here</p>
+                            <p className="mt-2 text-base text-gray-300">
+                            Only jpeg, png & mp3 files supported</p>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     );
