2f13845658490a07e6927d6c0ca8da5434e89426
Liedel <liedel75@gmail.com>
2022-06-29 16:22:45 +0200

    fix syntax upload controller download link

--- client/pages/download/[id]/index.tsx
+++ client/pages/download/[id]/index.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import {GetServerSidePropsContext, NextPage} from "next";
+import axios from "axios";
+import {IFile} from "../../../libs/types";
 
-const Index = () => {
+const index: NextPage<{
+    file: IFile
+}> = ({ file }) => {
     return (
-        <div>
-           download page
+        <div className="text-white">
+            <p className="text-white">{file.name}</p>
         </div>
     );
 };
 
-export default Index;
+export default index;
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+    const {id} = context.query;
+    let file;
+    try {
+        const { data } = await axios.get(`http://localhost:5001/api/files/${id}`);
+        file = data;
+        console.log("next file data", data)
+    } catch (error) {
+        console.log(error.response.data);
+        file = {};
+    }
+
+    return {
+        props: {
+            file,
+        }
+    }
+}
