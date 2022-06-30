92aaa7dd3c6794c6e8aeb0c07f0929398db1e5c2
Liedel <liedel75@gmail.com>
2022-06-30 18:34:23 +0200

    add nodemailer, Sendinblue & route

--- server/controllers/upload.controller.ts
+++ server/controllers/upload.controller.ts
@@ -1,8 +1,10 @@
 import { UploadApiResponse, v2 as cloudinary } from "cloudinary";
 import {Request, Response} from "express";
 import File from "../models/file";
+const nodemailer = require("nodemailer");
 
 import https from "https"
+import createEmailTemplate from "../utils/createEmailTemplate";
 
 module.exports.uploadPost = async (req: Request, res: Response)=> {
     try {
@@ -77,4 +79,59 @@ module.exports.downloadFile = async (req:Request, res:Response)=> {
     } catch(err) {
         return res.status(500).json({message: "Server Error file"})
     }
+}
+
+/*Email*/
+module.exports.emails = async(req:Request, res:Response)=> {
+    // 1. Validate request
+    const {id, emailFrom, emailTo} = req.body
+    // 2. File exist ?
+    const file = await File.findById(id)
+    if(!file) {
+        return res.status(404).json({message: "File doesn't exist"})
+    }
+    // 3. Create transporter
+    let transporter = nodemailer.createTransport({
+        host: process.env.SENDINBLUE_SMTP_HOST,
+        port: process.env.SENDINBLUE_SMTP_PORT,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: process.env.SENDINBLUE_SMTP_USER,
+            pass: process.env.SENDINBLUE_SMTP_PASSWORD,
+        },
+    });
+
+    // 4. Email data
+    const { filename, sizeInBytes } = file;
+    const filesize = `${(Number(sizeInBytes) / (1024 * 1024)).toFixed(2)} MB`;
+    const downloadPageLink = `${process.env.API_ENDPOINT_CLIENT}download/${id}`;
+
+    const mailOptions = {
+        from: emailFrom, // sender address
+            to: emailTo, // list of receivers
+        subject: "File shared ✔", // Subject line
+        text: `${emailFrom} shared a file with you`, // plain text body
+        html:createEmailTemplate(emailFrom,downloadPageLink,filename,filesize), // html body
+    }
+
+    // 5. Send mail using the transporter
+    transporter.sendMail(mailOptions, async(error: any, info: any)=> {
+        if(error) {
+            console.log(error);
+            return res.status(500).json({
+                message: "server error !"
+            });
+        }
+
+        file.sender = emailFrom;
+        file.receiver = emailTo;
+
+        await file.save();
+        return res.status(200).json({
+            message:"Email sent",
+        })
+    })
+
+
+    // 6. Save the data and send the response
 }
--- server/routes/files.ts
+++ server/routes/files.ts
@@ -7,14 +7,10 @@ const uploadController = require('../controllers/upload.controller');
 const storage = multer.diskStorage({})
 let upload = multer({storage});
 
-/*Upload*/
 router.post("/upload", upload.single("myFile"), uploadController.uploadPost);
-
-/*Download link*/
 router.get("/:id", uploadController.downloadLink)
-
-/*Download file*/
 router.get("/:id/download", uploadController.downloadFile)
+router.post("/email", uploadController.emails)
 
 module.exports = router;
 
